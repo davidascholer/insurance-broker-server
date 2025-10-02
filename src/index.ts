5f42564ad7d77e58f31d2b80e8d87ecc22b289bf
David Scholer <davidascholer@gmail.com>
2025-10-02 01:23:17 -0700

    cors edit

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import authRouter from "./routes/authRouter";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 
 // Routes
