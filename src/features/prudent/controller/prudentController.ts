16e9152dd2fc1aacfb4dca4dfb70786e18daf574
David Scholer <davidascholer@gmail.com>
2025-10-14 17:13:41 -0700

    prudent to prod

--- src/features/prudent/controller/prudentController.ts
+++ src/features/prudent/controller/prudentController.ts
@@ -14,57 +14,75 @@ import { PrudentSingleQuoteType } from "../types/PrudentResponseType";
  * @returns the data in Prudent format
  */
 export const getPrudentData = async (req, res) => {
-  const apiKey = process.env.PRUDENT_API_KEY;
-  const code = process.env.PRUDENT_CODE;
-  if (!apiKey) {
-    return res.status(500).send("Prudent API key not configured");
-  }
-  if (!code) {
-    return res.status(500).send("Prudent code not configured");
-  }
-
-  // Map the request body to Prudent format
-  const reqBody: PrudentRequestType = mapPipaRequestToPrudentRequest(req.body);
-  // // Verify the request body
-  // const reqIsValid = verifyPrudentRequest(reqBody);
-  // if (!reqIsValid) {
-  //   return res.status(400).send("Request body failed verification");
-  // }
-
-  const response = await fetch(
-    `https://quote-dev.prudentpet.com/api/quoting/quote/all?code=${code}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify(reqBody),
+  try {
+    const apiKey = process.env.PRUDENT_API_KEY;
+    const code = process.env.PRUDENT_CODE;
+    if (!apiKey) {
+      return res.status(500).send("Prudent API key not configured");
     }
-  );
+    if (!code) {
+      return res.status(500).send("Prudent code not configured");
+    }
 
-  if (!response.ok) {
-    console.error("Error fetching Prudent data:", response.statusText);
-    return res.status(500).send("Error fetching Prudent data");
+    // Map the request body to Prudent format
+    const reqBody: PrudentRequestType = mapPipaRequestToPrudentRequest(
+      req.body
+    );
+    // // Verify the request body
+    // const reqIsValid = verifyPrudentRequest(reqBody);
+    // if (!reqIsValid) {
+    //   return res.status(400).send("Request body failed verification");
+    // }
+
+    const response = await fetch(
+      `https://quote.prudentpet.com/api/quoting/quote/all?code=${code}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify(reqBody),
+      }
+    );
+    // console.log("Request sent to Prudent API:", reqBody);
+    console.log("Response received from Prudent API:", response);
+
+    if (!response.ok) {
+      console.error("Error fetching Prudent data:", response.statusText);
+      return res.status(500).send("Error fetching Prudent data");
+    }
+
+    if (response.headers.get("content-type")?.includes("text/html")) {
+      const text = await response.text();
+      console.error("HTML response received from Prudent API:", text);
+      return res.status(500).send("Unexpected HTML response from Prudent API");
+    }
+    // console.log("Response body received from Prudent API:", response.body);
+    const data = await response.json();
+    console.log("Data received from Prudent API:", data);
+
+    // // Write the object to a JSON file
+    // const filePath = "output/test_prudent_data.json";
+    // fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf8", (err) => {
+    //   if (err) {
+    //     console.error("Error writing file:", err);
+    //     return;
+    //   }
+    //   console.log(`Object successfully written to ${filePath}`);
+    // });
+
+    const resBody = mapPrudentResponseToPipaResponse({
+      pipaData: req.body,
+      prudentData: data,
+    });
+    res.send({ data: resBody });
+  } catch (error) {
+    console.error("Unexpected error in getPrudentData:", error);
+    return res
+      .status(500)
+      .send("An error occurred. Possibly an invalid API key.");
   }
-  const data = await response.json();
-  console.log("Response received from Prudent API:", data);
-
-  // // Write the object to a JSON file
-  // const filePath = "output/test_prudent_data.json";
-  // fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf8", (err) => {
-  //   if (err) {
-  //     console.error("Error writing file:", err);
-  //     return;
-  //   }
-  //   console.log(`Object successfully written to ${filePath}`);
-  // });
-
-  const resBody = mapPrudentResponseToPipaResponse({
-    pipaData: req.body,
-    prudentData: data,
-  });
-  res.send({ data: resBody });
 };
 
 /**
@@ -87,7 +105,7 @@ export const getSinglePrudentQuote = async (req, res) => {
     return res.status(400).send("No quote data provided");
 
   const response = await fetch(
-    `https://quote-dev.prudentpet.com/api/quoting/quote?code=${code}`,
+    `https://quote.prudentpet.com/api/quoting/quote?code=${code}`,
     {
       method: "POST",
       headers: {
