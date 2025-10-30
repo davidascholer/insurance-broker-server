833eb45af4ff724da5ae364937cd3752fd6eeb10
David Scholer <davidascholer@gmail.com>
2025-10-30 13:10:51 -0700

    kanguruController edit

--- src/features/kanguro/controller/kanguroController.ts
+++ src/features/kanguro/controller/kanguroController.ts
@@ -34,52 +34,52 @@ export const getKanguroData = async (req, res) => {
     },
     body: JSON.stringify(reqBody),
   };
-  // request(options, function (error, response) {
-  //   if (error) {
-  //     console.error("Error fetching Prudent data:", response);
-  //     return res.status(500).send("Error fetching Prudent data");
-  //   }
-  //   // console.log("Kanguro Response Body:", response.body);
-  //   const resBody = mapKanguroResponseToPipaResponse({
-  //     pipaData: req.body,
-  //     kanguroData: JSON.parse(response.body),
-  //   });
-  //   res.send({ data: resBody });
-  // });
-
-  const response = await fetch(
-    `https://kanguro-integrated-api-staging.azurewebsites.net/api/pet/quote`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "X-API-Key": apiKey,
-      },
-      body: JSON.stringify(reqBody),
+  request(options, function (error, response) {
+    if (error) {
+      console.error("Error fetching Prudent data:", response);
+      return res.status(500).send("Error fetching Prudent data", error);
     }
-  );
-
-  if (!response.ok) {
-    console.error("Error fetching Prudent data:", response);
-    return res.status(500).send("Error fetching Prudent data");
-  }
-  const data = await response.json();
-  console.log("Kanguro Response data:", JSON.stringify(data));
-
-  // // Write the object to a JSON file
-  // const filePath = "output/test_kanguro_data.json";
-  // fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf8", (err) => {
-  //   if (err) {
-  //     console.error("Error writing file:", err);
-  //     return;
-  //   }
-  //   console.log(`Object successfully written to ${filePath}`);
-  // });
-
-  const resBody = mapKanguroResponseToPipaResponse({
-    pipaData: req.body,
-    kanguroData: data,
+    // console.log("Kanguro Response Body:", response.body);
+    const resBody = mapKanguroResponseToPipaResponse({
+      pipaData: req.body,
+      kanguroData: JSON.parse(response.body),
+    });
+    res.send({ data: resBody });
   });
-  res.send({ data: resBody });
+
+  // const response = await fetch(
+  //   `https://kanguro-integrated-api-staging.azurewebsites.net/api/pet/quote`,
+  //   {
+  //     method: "POST",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       "X-API-Key": apiKey,
+  //     },
+  //     body: JSON.stringify(reqBody),
+  //   }
+  // );
+
+  // if (!response.ok) {
+  //   console.error("Error fetching Prudent data:", response);
+  //   return res.status(500).send("Error fetching Prudent data");
+  // }
+  // const data = await response.json();
+  // console.log("Kanguro Response data:", JSON.stringify(data));
+
+  // // // Write the object to a JSON file
+  // // const filePath = "output/test_kanguro_data.json";
+  // // fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf8", (err) => {
+  // //   if (err) {
+  // //     console.error("Error writing file:", err);
+  // //     return;
+  // //   }
+  // //   console.log(`Object successfully written to ${filePath}`);
+  // // });
+
+  // const resBody = mapKanguroResponseToPipaResponse({
+  //   pipaData: req.body,
+  //   kanguroData: data,
+  // });
+  // res.send({ data: resBody });
 };
